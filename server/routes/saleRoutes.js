c3c1371d7c8ae73de86dc6c282274ce2babd80a0
david201020 <136210225+david201020@users.noreply.github.com>
2024-11-25 20:37:46 -0500

    back

--- server/routes/saleRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createVenta, sales} =require('../controllers/sale')
-
-router.post("/create", createVenta);
-router.get("/all", sales)
-
-
-module.exports = router;
